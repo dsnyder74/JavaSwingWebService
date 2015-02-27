e48d1b41154c096ad62afc2078f8c49a309418f0
David Snyder <dsnyder74@gmail.com>
2015-02-27 00:09:31 -0500

    Add Hibernate support for listing all shopkins

--- WebContent/WEB-INF/jsp/shopkin-page.jsp
+++ WebContent/WEB-INF/jsp/shopkin-page.jsp
@@ -1,7 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"  
     pageEncoding="ISO-8859-1"%>  
 <%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
-
+<%@ taglib prefix="c" 
+           uri="http://java.sun.com/jsp/jstl/core" %>
+           
 <html>
 <head>
 <title>Shopkin Entry</title>
@@ -24,6 +26,15 @@ body {
 					<tr><td colspan=2><input type="submit" name="submit" value="Submit" /></td></tr>
 				</table>
 			</form>
+		</div>
+		
+		<div>
+			<table>
+				<tr><td>Shopkin ID</td><td>Shopkin Name</td></tr>
+				<c:forEach var="shopkin" items="${allshopkins}" varStatus="status">
+ 				<tr><td>${shopkin.id}</td><td>${shopkin.name}</td></tr>
+				</c:forEach>
+			</table>
 		</div>
 		
 	</div>
--- src/dgs/projects/shopkinsdb/ShopkinsDB.java
+++ src/dgs/projects/shopkinsdb/ShopkinsDB.java
@@ -1,28 +1,50 @@
 package dgs.projects.shopkinsdb;
 
-import java.util.Map;
-
-import javax.swing.text.html.HTMLDocument.Iterator;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import dgs.projects.shopkinsdb.util.HibernateUtil;
+import org.hibernate.Criteria;
 
 @Controller
 public class ShopkinsDB {
 	
 	@RequestMapping("/")
 	public ModelAndView addShopkin1() {
-		return new ModelAndView("shopkin-page", "shopkin-entity", new Shopkin());
+		
+		//ModelAndView model = new ModelAndView("shopkin-page", "shopkin-entity", new Shopkin());
+        ModelAndView modelAndView = new ModelAndView();  
+        modelAndView.setViewName("shopkin-page");
+        modelAndView.addObject("shopkin-entity", new Shopkin());
+        
+    	Transaction trns = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			trns = session.beginTransaction();
+			
+	        @SuppressWarnings("unchecked")
+	        List<Shopkin> listUser = (List<Shopkin>) session
+	                .createCriteria(Shopkin.class)
+	                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
+	        
+			modelAndView.addObject("allshopkins", listUser);
+			
+		} catch (RuntimeException e) {
+			if(trns != null){
+				trns.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			session.flush();
+			session.close();
+		} 
+		
+		return modelAndView;
 	}
 	
     @RequestMapping(value="/process-shopkin")  
